f31a8ce1dd4463368eaed22eca3d238701df511e
imtyaz <imtiyaz@itconquest.com>
2025-03-22 23:30:22 +0600

    websockets broadcasting FE setup

---
+++ app/Events/NewTrade.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class NewTrade implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $trade;
+    public function __construct($trade)
+    {
+        $this->trade = $trade;
+        Log::info('NewTrade event fired with trade: ' . $trade);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('trades');
+    }
+
+}
